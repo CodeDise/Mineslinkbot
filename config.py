2ee9c58770b326f1bb81aebd19985afd161250f0
CODEDISE <otxstudioofficial@gmail.com>
2025-12-04 01:01:12 +0530

    Update DATABASE_URL in config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6888478102")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kojar29650:h8jvNvpyteVB8AzM@cluster0.uywzxza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kojar29650:h8jvNvpyteVB8AzM@cluster0.uywzxza.mongodb.net/?retryWrites=true&w=majority&appName=Mineslinkbot")
 DB_NAME = os.environ.get("DATABASE_NAME", "Diablo")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "20"))  # 0 means no expiry
